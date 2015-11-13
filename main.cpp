f0fea3305ad568ceee4335233825294fa31f447f
Sebastian Meyer <archi@users.noreply.github.com>
2015-11-13 10:31:20 +0100

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,11 @@
+/**
+ * This will NOT build!
+ *
+ * Protocol Ref:
+ *   http://volvo.wot.lv/wiki/doku.php?id=melbus
+ */
+ 
+#include "devices.h"
+#include "cmds.h"
+#include "comm.h"
+#include "protocol.h"
